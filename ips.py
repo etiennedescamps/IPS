2cfcb322c3d782dd05408769e3d58cd0c590d71c
etiennedescamps <55227616+etiennedescamps@users.noreply.github.com>
2020-04-21 04:09:27 +0200

    Update ips.py

--- ips.py
+++ ips.py
@@ -6,7 +6,7 @@ dos_warning = False
 blacklist = []
 
 def pkt_detection(packet):
-    key = (packet[0][1].src, packet[0][1].dst)
+    key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
     packet_counts.update([key])
     return ("Packet detected: {} ==> {}".format(packet[0][1].src, packet[0][1].dst))
 
@@ -14,9 +14,9 @@ def pkt_detection(packet):
 while True:
     print ("Listening...")
     packet_counts = Counter()
-    sniff(lfilter=lambda pkt: IP in pkt and pkt[IP].src not in blacklist, prn=pkt_detection, timeout=1, iface=["eth0", "eth1", "eth2"])
+    sniff(lfilter=lambda pkt: IP in pkt and pkt[IP].src not in blacklist, prn=pkt_detection, timeout=1)
     for key, count in packet_counts.items():
-        if count >= 5:
+        if count >= 10:
             if dos_warning == False:
                 dos_warning = True
                 print ("WARNING: {} might currently be under DoS attack from {}.".format(key[1], key[0]))
