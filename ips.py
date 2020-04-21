0c463d8ed3968ad41ce1513478856732a5523a27
etiennedescamps <55227616+etiennedescamps@users.noreply.github.com>
2020-04-21 03:31:45 +0200

    Update ips.py

--- ips.py
+++ ips.py
@@ -3,10 +3,9 @@ from scapy.all import sniff, IP
 from scapy import *
 
 dos_warning = False
-dos_attack = False
 blacklist = []
 
-def custom_action(packet):
+def pkt_detection(packet):
     key = (packet[0][1].src, packet[0][1].dst)
     packet_counts.update([key])
     return ("Packet detected: {} ==> {}".format(packet[0][1].src, packet[0][1].dst))
@@ -15,18 +14,17 @@ def custom_action(packet):
 while True:
     print ("Listening...")
     packet_counts = Counter()
-    sniff(lfilter=lambda pkt: IP in pkt and pkt[IP].src not in blacklist, prn=custom_action, timeout=1, iface=["eth0", "eth1"])
+    sniff(lfilter=lambda pkt: IP in pkt and pkt[IP].src not in blacklist, prn=pkt_detection, timeout=1, iface=["eth0", "eth1"])
     for key, count in packet_counts.items():
-        if count >= 3:
+        if count >= 5:
             if dos_warning == False:
                 dos_warning = True
-                print ("WARNING: risk of {} currently being under DoS attack from {}.".format(key[1], key[0]))
+                print ("WARNING: {} might currently be under DoS attack from {}.".format(key[1], key[0]))
             else:
-                dos_attack = True
                 print ("WARNING: DoS attack from {} on {} confirmed, communication from {} will be ignored.".format(key[0], key[1], key[0]))
                 blacklist.append(key[0])
                 dos_warning = False
                 print ("WARNING: {} is now ignored by the IPS, resuming listening.".format(key[0]))
         elif dos_warning == True:
             dos_warning = False
-            print ("Risk of DoS went unconfirmed, all clear.")
+            print ("Risk of DoS attack went unconfirmed, all clear.")
